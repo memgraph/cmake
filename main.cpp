357f471c8262164c2e5a0ef4b2364d71ae3245a0
Marko Budiselic <marko.budiselic@memgraph.com>
2022-01-26 19:13:14 +0100

    Add working boost example (still not good enough

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
+#include <boost/chrono.hpp>
+#include <boost/thread.hpp>
+
+void wait(int seconds) {
+  boost::this_thread::sleep_for(boost::chrono::seconds{seconds});
+}
+
+void thread() {
+  for (int i = 0; i < 5; ++i) {
+    wait(1);
+    std::cout << i << '\n';
+  }
+}
+
 int main() {
+  std::cout << boost::chrono::system_clock::now() << std::endl;
+  boost::thread t{thread};
+  t.join();
   return 0;
 }
--- modules/external-boost.cmake
+++ modules/external-boost.cmake
@@ -1,23 +1,55 @@
-set(BOOST_MODULES "" CACHE STRING "Picks Boost modules separated by space")
-if("${BOOST_MODULES}" STREQUAL "")
-  message(FATAL_ERROR "Some Boost modules have to be picked")
-endif()
 set(BOOST_GIT_TAG "boost-1.78.0" CACHE STRING "Boost git tag")
 
 set(boost_URL "https://github.com/boostorg/boost.git" )
 set(boost_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/third_party/boost)
-set(boost_INCLUDE_DIR ${Boost_INSTALL}/include)
-set(boost_LIB_DIR ${Boost_INSTALL}/lib)
+set(boost_INCLUDE_DIR ${boost_INSTALL_DIR}/include)
+set(boost_LIB_DIR ${boost_INSTALL_DIR}/lib)
 set(boost_LIBRARY_SUFFIX .a)
 
-string(REPLACE " " ";" BOOST_MODULES_LIST ${BOOST_MODULES})
-string(APPEND boost_CONFIGURED_MODULES "")
-foreach(boost_MODULE ${BOOST_MODULES_LIST})
-  string(APPEND boost_CONFIGURED_MODULES " --with-libraries=${boost_MODULE} ")
-  add_library(boost::${boost_MODULE} STATIC IMPORTED)
-  set_property(TARGET boost::${boost_MODULE} PROPERTY IMPORTED_LOCATION ${boost_LIB_DIR}/libboost_${boost_MODULE}${boost_LIBRARY_SUFFIX})
-  set_property(TARGET boost::${boost_MODULE} PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${boost_INCLUDE_DIR})
-  add_dependencies(boost::${boost_MODULE} boost-proj)
+add_library(boost_headers INTERFACE)
+target_include_directories(boost_headers INTERFACE ${boost_INCLUDE_DIR})
+list(APPEND boost_MODULES
+  "atomic"
+  "chrono"
+  "container"
+  "context"
+  "contract"
+  "coroutine"
+  "date_time"
+  "exception"
+  "filesystem"
+  "graph"
+  "iostreams"
+  "json"
+  "locale"
+  "log"
+  "log_setup"
+  "math_c99"
+  "math_c99f"
+  "math_c99l"
+  "math_tr1"
+  "math_tr1f"
+  "math_tr1l"
+  "nowide"
+  "prg_exec_monitor"
+  "program_options"
+  "python38"
+  "random"
+  "regex"
+  "serialization"
+  "system"
+  "text_exec_minitor"
+  "thread"
+  "timer"
+  "type_erasure"
+  "unit_test_framework"
+  "wave"
+  "wserialization"
+)
+foreach(boost_MODULE ${boost_MODULES})
+  add_library(boost_${boost_MODULE}_static STATIC IMPORTED)
+  set_property(TARGET boost_${boost_MODULE}_static PROPERTY IMPORTED_LOCATION ${boost_LIB_DIR}/libboost_${boost_MODULE}${boost_LIBRARY_SUFFIX})
+  add_dependencies(boost_${boost_MODULE}_static boost-proj)
 endforeach()
 
 ExternalProject_Add(boost-proj
@@ -26,7 +58,7 @@ ExternalProject_Add(boost-proj
   PREFIX boost-proj
   BUILD_IN_SOURCE 1
   INSTALL_DIR ${boost_INSTALL_DIR}
-  CONFIGURE_COMMAND ./bootstrap.sh ${boost_CONFIGURED_MODULES} --prefix=<INSTALL_DIR>
+  CONFIGURE_COMMAND ./bootstrap.sh --prefix=<INSTALL_DIR>
   BUILD_COMMAND ./b2 install link=static variant=release threading=multi runtime-link=static
   INSTALL_COMMAND ""
 )
