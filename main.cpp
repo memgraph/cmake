9dd9c23c4c42c657cbadd311bbee3a31392e552d
Marko Budiselic <marko.budiselic@memgraph.com>
2022-01-26 22:20:38 +0100

    Fix some issues

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+#include <nlohmann/json.hpp>
+
 int main() {
   std::cout << "Testing CMake libs" << std::endl;
+
+  nlohmann::json j;
+  j["a"] = "b";
+  std::cout << j << std::endl;
+
   return 0;
 }
