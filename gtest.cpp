b0173a726d1b7268036da45243327e1fc46d1bb2
Marko Budiselic <mbudiselicbuda@gmail.com>
2023-12-23 12:36:32 -0500

    Improve gtest setup

---
+++ gtest.cpp
@@ -0,0 +1,3 @@
+#include "gtest/gtest.h"
+
+TEST(example_test_case, example_test) { EXPECT_EQ(1, 1); }
--- modules/fetch-gtest.cmake
+++ modules/fetch-gtest.cmake
@@ -1,4 +1,8 @@
-set(GTEST_GIT_TAG "release-1.11.0" CACHE STRING "GTest git tag")
+if(NOT DEFINED GTEST_GIT_TAG)
+  set(GTEST_GIT_TAG "v1.14.0" CACHE STRING "GTest git tag")
+else()
+  set(GTEST_GIT_TAG "${GTEST_GIT_TAG}" CACHE STRING "GTest git tag")
+endif()
 
 FetchContent_Declare(googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
