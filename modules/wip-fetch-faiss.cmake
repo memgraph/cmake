0e979f8644fdb789338871b9aa4283013eacf25d
Marko Budiselic <marko.budiselic@memgraph.com>
2024-08-18 16:35:34 +0200

    Fix faiss

--- modules/wip-fetch-faiss.cmake
+++ modules/wip-fetch-faiss.cmake
@@ -5,8 +5,27 @@ else()
 endif()
 
 FetchContent_Declare(faiss
-  GIT_REPOSITORY    https://github.com/facebookresearch/faiss.git
-  GIT_TAG           ${FAISS_GIT_TAG}
-  CMAKE_ARGS        -DFAISS_ENABLE_GPU=OFF
+  GIT_REPOSITORY     https://github.com/facebookresearch/faiss.git
+  GIT_TAG            ${FAISS_GIT_TAG}
 )
+# Install instructions https://github.com/facebookresearch/faiss/wiki/Installing-Faiss
+# Flags https://github.com/facebookresearch/faiss/blob/main/INSTALL.md
+# NOTE: OpenMP is required:
+#   * MacOS: brew install libomp
+#            set(OpenMP_ROOT /opt/homebrew/opt/libomp) | -DOpenMP_ROOT=/opt/homebrew/opt/libomp
+set(CMAKE_BUILD_TYPE    Release)
+set(BUILD_TESTING       OFF)
+set(FAISS_ENABLE_GPU    OFF)
+set(FAISS_ENABLE_C_API  OFF)
+set(FAISS_ENABLE_PYTHON OFF)
+set(FAISS_ENABLE_RAFT   OFF)
+# FAISS_OPT_LEVEL (https://github.com/facebookresearch/faiss/blob/main/INSTALL.md#step-1-invoking-cmake):
+#   * x86_64:  generic, avx2, avx512
+#   * aarch64: generic, sve
+if("${CMAKE_HOST_SYSTEM_PROCESSOR}" STREQUAL "arm64")
+  set(FAISS_OPT_LEVEL   sve)
+  message(STATUS "Detected ${CMAKE_HOST_SYSTEM_PROCESSOR} setting FAISS_OPT_LEVEL ${FAISS_OPT_LEVEL}")
+else()
+  set(FAISS_OPT_LEVEL   generic)
+endif()
 FetchContent_MakeAvailable(faiss)
--- vector_search.cpp
+++ vector_search.cpp
@@ -1,11 +1,17 @@
-// NOTE: From https://github.com/unum-cloud/usearch/tree/main/cpp
+#include <cstdio>
+#include <cstdlib>
+#include <random>
 
+// annoy
 #include <annoylib.h>
 #include <kissrandom.h>
-
+// faiss
+#include <faiss/IndexHNSW.h>
+// usearch
 #include <usearch/index.hpp>
 #include <usearch/index_dense.hpp>
 
+using idx_t = faiss::idx_t;
 using namespace unum::usearch;
 
 void run_annoy() {
@@ -15,6 +21,53 @@ void run_annoy() {
                             Annoy::AnnoyIndexMultiThreadedBuildPolicy>(40);
 }
 
+// https://github.com/facebookresearch/faiss/blob/main/tutorial/cpp/6-HNSW.cpp
+void run_faiss() {
+    int d = 64;      // dimension
+    int nb = 100000; // database size
+    int nq = 10000;  // nb of queries
+    std::mt19937 rng;
+    std::uniform_real_distribution<> distrib;
+    float* xb = new float[d * nb];
+    float* xq = new float[d * nq];
+    for (int i = 0; i < nb; i++) {
+        for (int j = 0; j < d; j++)
+            xb[d * i + j] = distrib(rng);
+        xb[d * i] += i / 1000.;
+    }
+    for (int i = 0; i < nq; i++) {
+        for (int j = 0; j < d; j++)
+            xq[d * i + j] = distrib(rng);
+        xq[d * i] += i / 1000.;
+    }
+    int k = 4;
+    faiss::IndexHNSWFlat index(d, 32);
+    index.add(nb, xb);
+
+    { // search xq
+        idx_t* I = new idx_t[k * nq];
+        float* D = new float[k * nq];
+        index.search(nq, xq, k, D, I);
+        printf("I=\n");
+        for (int i = nq - 5; i < nq; i++) {
+            for (int j = 0; j < k; j++)
+                printf("%5lld ", I[i * k + j]);
+            printf("\n");
+        }
+        printf("D=\n");
+        for (int i = nq - 5; i < nq; i++) {
+            for (int j = 0; j < k; j++)
+                printf("%5f ", D[i * k + j]);
+            printf("\n");
+        }
+        delete[] I;
+        delete[] D;
+    }
+    delete[] xb;
+    delete[] xq;
+}
+
+// NOTE: From https://github.com/unum-cloud/usearch/tree/main/cpp
 void run_usearch() {
   metric_punned_t metric(3, metric_kind_t::l2sq_k, scalar_kind_t::f32_k);
   // If you plan to store more than 4 Billion entries - use `index_dense_big_t`.
@@ -36,6 +89,7 @@ void run_usearch() {
 
 int main(int argc, char **argv) {
   run_annoy();
+  run_faiss();
   run_usearch();
   return 0;
 }
