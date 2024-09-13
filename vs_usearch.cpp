e4673cabaac39e3443033ffa509e21e16394b494
Marko Budiselić <marko.budiselic@memgraph.com>
2024-09-13 18:07:57 +0200

    Extend vector search experiment (#20)

--- vs_usearch.cpp
+++ vs_usearch.cpp
@@ -1,5 +1,6 @@
 #include <cstdint>
 #include <iostream>
+#include <thread>
 
 // CPP: https://github.com/unum-cloud/usearch/tree/main/cpp
 // REF: https://unum-cloud.github.io/usearch/cpp/reference.html
@@ -31,33 +32,51 @@ template <> struct unum::usearch::hash_gt<std::pair<uint64_t, uint64_t>> {
 };
 
 void run_usearch() {
-  metric_punned_t metric(1000, metric_kind_t::l2sq_k, scalar_kind_t::f32_k);
+  uint64_t vector_size = 1000;
+  metric_punned_t metric(vector_size, metric_kind_t::l2sq_k,
+                         scalar_kind_t::f32_k);
+  uint64_t index_size = 10;
   // using mg_key_t = uint64_t;
-  // // NOTE: To make std::string work as a key, I had to replace built-in exchange
+  // // NOTE: To make std::string work as a key, I had to replace built-in
+  // exchange
   // // (C++11 compatiblity) function by std::exchange.
   // using mg_key_t = std::string;
   // using mg_key_t = std::pair<uint64_t, uint64_t>;
   using mg_key_t = mg_tx_data_t;
   using mg_vector_index_t = index_dense_gt<mg_key_t, uint40_t>;
   mg_vector_index_t index = mg_vector_index_t::make(metric);
-  index.reserve(10);
-  for (int i = 0; i < 10; ++i) {
-    std::vector<float> vec(1000, 0.0);
-    for (int j = 0; j < 1000; ++j) {
-      vec[j] = i + j;
+  // std::mt19937 rng;
+  // std::uniform_real_distribution<> distrib;
+  index.reserve(index_size);
+  for (int i = 0; i < index_size; ++i) {
+    std::vector<float> indexed_vector(vector_size, 0.0);
+    for (int j = 0; j < vector_size; ++j) {
+      indexed_vector[j] = i + j;
+      // indexed_vector[j] = distrib(rng);
     }
-    // index.add(i, vec.data());
-    // index.add(std::make_pair(i, i), vec.data());
-    // index.add("key" + std::to_string(i), vec.data());
-    index.add(mg_tx_data_t{.gid = (uint64_t)i, .tx = 0, .cmd = 0}, vec.data());
+    // index.add(i, indexed_vector.data());
+    // index.add(std::make_pair(i, i), indexed_vector.data());
+    // index.add("key" + std::to_string(i), indexed_vector.data());
+    index.add(mg_tx_data_t{.gid = (uint64_t)i, .tx = 0, .cmd = 0},
+              indexed_vector.data());
+    // NOTE: At 100k of vector of size 1000, RES was ~410MB (on M1).
+    // TODO(gitbuda): Estimate/figure_out ratio between in-memory data structure
+    // vs raw size of the vectors.
+    if (i != 0 && i % 10000 == 0) {
+      std::cout << 10000 << " of more vectors added. Now " << i
+                << " vectors are indexed" << std::endl;
+    }
+  }
+  std::cout << "Index created." << std::endl;
+  // std::this_thread::sleep_for (std::chrono::seconds(100)); // A hack to
+  // observe RAM growth.
+
+  std::vector<float> query_vector(vector_size, 0.0);
+  for (int j = 0; j < vector_size; ++j) {
+    query_vector[j] = j;
   }
 
-  std::vector<float> vec(1000, 0.0);
-  for (int j = 0; j < 1000; ++j) {
-    vec[j] = j;
-  }
-
-  auto results = index.search(vec.data(), 5);
+  auto results = index.search(query_vector.data(), 5);
   for (std::size_t i = 0; i != results.size(); ++i) {
     auto key = static_cast<mg_key_t>(results[i].member.key);
     auto distance = results[i].distance;
